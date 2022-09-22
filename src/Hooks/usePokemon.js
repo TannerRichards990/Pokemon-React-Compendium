c8a036d799530ea92f76d554640de3b66b6f01eb
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-22 12:59:20 -0700

    built out custom hook needed for grabbing info

--- src/Hooks/usePokemon.js
+++ src/Hooks/usePokemon.js
@@ -8,10 +8,36 @@ export default function usePokemon() {
   const [searchPokemon, setSearchPokemon] = useState('');
   const [selectedType, setSelectedType] = useState('all');
   const [types, setTypes] = useState([]);
-}
 
-useEffect(() => {
-  const fetchData = async (selectedType, searchPokemon) => {
-    try {
-      
-    }
+
+  useEffect(() => {
+    const fetchData = async (selectedType, searchPokemon) => {
+      try {
+        const fetchData = async (selectedType, searchPokemon) => {
+          setPokemonList(data);
+          setLoading(false);
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }; 
+
+    fetchData(selectedType, searchPokemon);
+  }, [selectedType, searchPokemon]);
+
+  useEffect(() => {
+    const fetchTypes = async () => {
+      try {
+        const data = await fetchTypes();
+        setTypes(data.map((type) => type.type));
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTypes();
+  }, []);
+
+  return { pokemonList, loading, types, selectedType, setSelectedType, searchPokemon };
+
+} 
