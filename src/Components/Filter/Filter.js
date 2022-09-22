a14783b5a317d727122a908921bbf3a1902e7b16
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-22 14:22:16 -0700

    built a Filter.js out, working on building that into the controls.js

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Filter({ types, selectedType, setSelectedType }) {
+  return (
+    <div className="filter">
+      <select value={selectedType} onChange={(e) => 
+        setSelectedType(e.target.value)}>
+        <option value="all">All</option>
+        {types.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
