273a5e869e7ea57719661ddbbf87c1f5307e4605
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-23 10:14:48 -0700

    added a feauture to type 2 on pokemon card, so type 2 will only display if it is relevant and not NA

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -9,12 +9,12 @@ export default function PokemonCard({ pokemon, url_image, type_1, type_2, hp }) 
       <div className="pokemon-card__info">
         <h3 className="pokemon-card__name">{pokemon}</h3>
         <div className="pokemon-card__types">
-          <div className="pokemon-card__type">{type_1}</div>
-          {type_2 && <div className="pokemon-card__type">{type_2}</div>}
+          <div className="pokemon-card__type">Type 1: {type_1}</div>
+          {type_2 !== 'NA' && <div className="pokemon-card__type">Type 2: {type_2}</div>}
         </div>
-        <div className="pokemon-card__stats">
-          <div className="pokemon-card__stat">
-            <div className="pokemon-card__stat-name">HP</div>
+        <div className="card-stats">
+          <div className="card__stat">
+            <div className="pokemon-card__stat-name">HP:</div>
             <div className="pokemon-card__stat-value">{hp}</div>
           </div>
         </div>
